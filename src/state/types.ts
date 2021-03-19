acc17555e7254652841a0ca2b5ae30b4569d3495
hachiojidev <hachiojidev@protonmail.com>
2021-03-19 16:50:21 +0900

    style: UI improvements

--- src/state/types.ts
+++ src/state/types.ts
@@ -180,6 +180,7 @@ export interface PredictionsState {
   currentEpoch: number
   isLoading: boolean
   isHistoryPaneOpen: boolean
+  isChartPaneOpen: boolean
   rounds: {
     [key: string]: RoundResponse
   }
